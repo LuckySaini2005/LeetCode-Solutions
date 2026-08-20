673eb0d75d0cb056b04f1cb3b4a3cc2e75bf2511
Lucky Saini <luckysaini1925@gmail.com>
2026-08-20 20:20:11 +0530

    Time: 0 ms (100%), Space: 10 MB (97.75%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -13,15 +13,18 @@ int helper(vector<int> &nums,vector<int> &dp,int idx){
 
     int rob(vector<int>& nums) {
         int n=nums.size();
-        vector<int>dp(n,-1);
+       
         if(n==1) return nums[0];
         if(n==2) return max(nums[0],nums[1]);
-        dp[0]=nums[0];
-        dp[1]=max(nums[0], nums[1]);
+         int prev1=nums[0];
+        int prev2=max(nums[0],nums[1]);
+        int curr;
         for(int i=2;i<n;i++){
-            dp[i]=max(nums[i]+dp[i-2],dp[i-1]);
+            curr=max(nums[i]+prev1,prev2);
+            prev1=prev2;
+            prev2=curr;
         }
-    return dp[n-1];    
+    return curr;    
     }
 };
 
