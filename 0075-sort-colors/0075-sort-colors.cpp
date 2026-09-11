54b09eed97ee24b624156436909ef1b2cd734726
Lucky Saini <luckysaini1925@gmail.com>
2026-09-12 00:03:08 +0530

    Time: 0 ms (100%), Space: 11.7 MB (48.96%) - LeetHub - Time: O(n), Space: O(1)

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,21 +1,27 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int zero=0;
-        int one=0;
-        int two=0;
-        for(int i =0;i<nums.size();i++){
-            if(nums[i]==0) zero++;
-            if(nums[i]==1) one++;
-            if(nums[i]==2) two++;
-        }
-        for(int i=0;i<nums.size();i++){
-            if(i<zero) nums[i]=0;
-            if(i>=zero && i<one+zero) nums[i]=1;
-            if(i>=one+zero) nums[i]=2;
-        }
+        int low = 0;
+        int mid = 0;
+        int high = nums.size() - 1;
 
+        while (mid <= high) {
 
+            if (nums[mid] == 0) {
+                swap(nums[low], nums[mid]);
+                low++;
+                mid++;
+            }
+
+            else if (nums[mid] == 1) {
+                mid++;
+            }
+
+            else {
+                swap(nums[mid], nums[high]);
+                high--;
+            }
+        }
     }
 };
 
