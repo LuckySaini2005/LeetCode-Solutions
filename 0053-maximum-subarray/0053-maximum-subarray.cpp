69012c6f82bc454a41f2624d9d3e83ee5c3fd393
Lucky Saini <luckysaini1925@gmail.com>
2026-09-12 14:41:24 +0530

    Time: 0 ms (100%), Space: 71.6 MB (81.21%) - LeetHub - Time: O(n), Space: O(1)

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,15 +1,25 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int current = nums[0];
-        int ans = nums[0];
+        // int current = nums[0];
+        // int ans = nums[0];
 
-        for (int i = 1; i < nums.size(); i++) {
-            current = max(nums[i], current + nums[i]);
-            ans = max(ans, current);
+        // for (int i = 1; i < nums.size(); i++) {
+        //     current = max(nums[i], current + nums[i]);
+        //     ans = max(ans, current);
+        // }
+
+        // return ans;
+        int maxi=INT_MIN;
+        int sum=nums[0];
+        maxi=max(maxi,sum);
+        for(int i=1;i<nums.size();i++){
+            if(sum<0) sum=0;
+            sum+=nums[i];
+             maxi=max(maxi,sum);
+            
         }
-
-        return ans;
+        return maxi;
     }
 };
 
